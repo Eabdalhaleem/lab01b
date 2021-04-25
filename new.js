aac94398f11f74c27dd08cbaa16c22165a40a709
Eabdalhaleem <eattaamneh@yahoo.com>
2021-04-25 17:34:49 +0300

    add lab1

---
+++ new.js
@@ -0,0 +1,8 @@
+let username = prompt("what is your name ")
+console.log(username);
+alert (" welcome " + username);
+
+let bookType = prompt(" please enter your book type ?")
+let bookNumber = prompt("how many book do you want?")
+let bookdeliver = prompt("do you want a deliver service (yes, no)")
+alert("we will waiting you" )
