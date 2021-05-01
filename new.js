40b0651f641000703bd2e4c8e5963644fd35ab34
Eabdalhaleem <eattaamneh@yahoo.com>
2021-05-01 06:22:55 +0300

    edited lab01b

--- new.js
+++ new.js
@@ -1,8 +1,22 @@
+'use strict';
+// ques1
 let username = prompt("what is your name ")
-console.log(username);
+console.log("what is your name " + username);
 alert (" welcome " + username);
 
+// ques2
 let bookType = prompt(" please enter your book type ?")
+console.log(" please enter your book type ?"+ bookType);
+alert ( bookType + "this is amazing choose");
+
+// ques3
 let bookNumber = prompt("how many book do you want?")
+console.log("how many book do you want?" + bookNumber);
+alert ("that\'s great");
+
+// ques4
 let bookdeliver = prompt("do you want a deliver service (yes, no)")
-alert("we will waiting you" )
+console.log( "do you want a deliver service (yes, no)" + bookdeliver);
+alert ("okay,we are here to help you");
+
+alert("we will waiting you" );
