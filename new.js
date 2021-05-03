0531b02da8932e424ed81f942306347ab44d5fd9
Eabdalhaleem <eattaamneh@yahoo.com>
2021-05-03 03:41:15 +0300

    adding and editing

--- new.js
+++ new.js
@@ -7,16 +7,17 @@ alert (" welcome " + username);
 // ques2
 let bookType = prompt(" please enter your book type ?")
 console.log(" please enter your book type ?"+ bookType);
-alert ( bookType + "this is amazing choose");
+alert (" you said your name \n" + username + " your book type is \n" + bookType  );
 
 // ques3
 let bookNumber = prompt("how many book do you want?")
 console.log("how many book do you want?" + bookNumber);
-alert ("that\'s great");
+alert (" you said your name \n"  + username  + " your book type is \n" + bookType   + "you order this mount of book \n" + bookNumber);
 
 // ques4
 let bookdeliver = prompt("do you want a deliver service (yes, no)")
 console.log( "do you want a deliver service (yes, no)" + bookdeliver);
-alert ("okay,we are here to help you");
+alert (" you said your name is : \n "  + username   + "\n your book type is : \n " + bookType + "\n you order this mount of book : \n" + bookNumber +
+"\n this you answer for delivery service \n" + bookdeliver );
 
 alert("we will waiting you" );
