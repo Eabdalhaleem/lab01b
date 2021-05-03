eb8623a9a661607684b35ae259759252b3511a51
Eabdalhaleem <eattaamneh@yahoo.com>
2021-05-04 02:30:50 +0300

    editing lab01b

--- new.js
+++ new.js
@@ -7,17 +7,17 @@ alert (" welcome " + username);
 // ques2
 let bookType = prompt(" please enter your book type ?")
 console.log(" please enter your book type ?"+ bookType);
-alert (" you said your name \n" + username + " your book type is \n" + bookType  );
+alert (" you said your name " + username + '\n' + " your book type is " + bookType  );
 
 // ques3
 let bookNumber = prompt("how many book do you want?")
 console.log("how many book do you want?" + bookNumber);
-alert (" you said your name \n"  + username  + " your book type is \n" + bookType   + "you order this mount of book \n" + bookNumber);
+alert (" you said your name "  + username  + '\n' +  " your book type is " +  bookType   +'\n' +  "you order this amount of book "  + bookNumber);
 
 // ques4
 let bookdeliver = prompt("do you want a deliver service (yes, no)")
 console.log( "do you want a deliver service (yes, no)" + bookdeliver);
-alert (" you said your name is : \n "  + username   + "\n your book type is : \n " + bookType + "\n you order this mount of book : \n" + bookNumber +
-"\n this you answer for delivery service \n" + bookdeliver );
+alert (" you said your name is :  "  + username   +'\n' +  " your book type is : " + bookType + '\n' +  " you order this mount of book : " + bookNumber + '\n' + 
+"this you answer for delivery service " + bookdeliver );
 
 alert("we will waiting you" );
